bf23764d4aef65a91af10f0d8c5718d13beeb9d2
Samuel Prevost <samuel.prevost@pm.me>
2020-11-02 18:38:59 +0100

    Added a PHP script to change Content Type

---
+++ vid/video_content_type.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Little script to:
+ *  1) Change the content type from application/octet-stream to video/mp4 so that 
+ *     the browser will play and not download the video
+ *  2) Download the requested video from Github
+ *  3) Serve it to the client in the response
+ *
+ *  This is to circumvent the Github limitation of serving videos as application/octet-stream
+ *  meaning our unit tests can't use them for testing.
+ *
+ *  Just host this script anywhere and call
+ *
+ *  https://anywhere.com/video_content_type.php?filename=VIDEO_FILENAME_HERE
+ *
+ *  THIS SCRIPT IS NOT SUPER SECURE, YOU SHOULD SANITIZE $_GET BEFORE USING IT !!!
+ *  Or you could also no publicly advertise that url... Your call
+ */
+
+header("Content-Type: video/mp4");
+// to understand why we need the false use_include_path=false
+// see https://stackoverflow.com/a/1336419/5989906
+echo(file_get_contents(
+    "https://github.com/sam1902/static-resources/raw/master/vid/".$_GET["filename"].".mp4", 
+    false));
